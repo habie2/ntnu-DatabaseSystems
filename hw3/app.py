043c77c28122ddabf604d22bc78f40357724d6f5
javichu <100472296@alumnos.uc3m.es>
2025-11-20 15:34:22 +0800

    update app.py to run with specified host and port configuration

--- hw3/app.py
+++ hw3/app.py
@@ -71,4 +71,7 @@ def water_plant(plant_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    
+    # host='0.0.0.0' visible externally
+    app.run(host='0.0.0.0', port=port)
